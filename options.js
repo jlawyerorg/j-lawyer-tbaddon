7763a711a34701f3b4cf98cd607fa4988cb48919
Maximilian Steinert <71747937+iradraconis@users.noreply.github.com>
2023-09-13 09:55:51 +0200

    Update options.js
    added license description

--- options.js
+++ options.js
@@ -1,3 +1,21 @@
+{/* j-Lawyer Thunderbird Extension - saves Messages to j-Lawyer Server Cases.
+Copyright (C) 2023, Maximilian Steinert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */}
+
+
+
 document.getElementById("saveButton").addEventListener("click", function() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
